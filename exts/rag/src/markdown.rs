4f704edca64499fa1989b395b39c140be05fac63
George MacKerron <george@mackerron.co.uk>
2024-10-04 14:46:39 +0100

    Now filtering script and style tags in markdown_from_html

--- exts/rag/src/markdown.rs
+++ exts/rag/src/markdown.rs
@@ -4,23 +4,25 @@ use pgrx::prelude::*;
 mod rag {
     use super::super::errors::*;
     use pgrx::prelude::*;
+    use htmd::*;
 
     #[pg_extern(immutable, strict)]
     pub fn markdown_from_html(document: &str) -> String {
-        htmd::convert(document).expect_or_pg_err("Error converting HTML to Markdown")
+        let converter = HtmlToMarkdown::builder().skip_tags(vec!["script", "style"]).build();
+        converter.convert(document).expect_or_pg_err("Error converting HTML to Markdown")
     }
 }
 
 #[cfg(any(test, feature = "pg_test"))]
 #[pg_schema]
 mod tests {
-    use pgrx::prelude::*;
     use super::rag::*;
+    use pgrx::prelude::*;
 
     #[pg_test]
     fn test_markdown_from_html() {
         assert_eq!(
-            markdown_from_html("<html><body><h1>Heading</h1><p>Para</p></body></html>"),
+            markdown_from_html("<html><script>alert();</script><body><h1>Heading</h1><p>Para</p></body></html>"),
             "# Heading\n\nPara"
         )
     }
