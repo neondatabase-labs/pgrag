38292abc976637ed3780a928c165c45105ce4185
George MacKerron <george@mackerron.co.uk>
2024-10-17 12:26:10 +0100

    Tidying

--- exts/rag_bge_small_en_v15/README.md
+++ exts/rag_bge_small_en_v15/README.md
@@ -1,3 +1,4 @@
+## Note on background workers
 
 Background workers must be initialized in the extension's `_PG_init()` function and can only be started if loaded through the `shared_preload_libraries` configuration setting. So to use this extension you'll need to edit `postgresql.conf` and, depending on the platform, add:
 
@@ -10,3 +11,7 @@ or
  ```
 shared_preload_libraries = 'rag_bge_small_en_v15.dylib'
 ```
+
+When using `cargo pgrx run` with Postgres instances installed by pgrx, the `postgresql.conf` file is located in `~/.pgrx/data-N` (where N is the relevant Postgres version).
+
+When using `cargo pgrx test`, the `postgresql.conf` file is inside the `target` directory of your extension, e.g. `~/path/to/myext/target/test-pgdata/N` (where N is the relevant Postgres version).
--- exts/rag_bge_small_en_v15/src/lib.rs
+++ exts/rag_bge_small_en_v15/src/lib.rs
@@ -1,3 +1,12 @@
+mod chunk;
+mod errors;
+mod embeddings {
+    tonic::include_proto!("embeddings");
+}
+
+use const_format::formatcp;
+use embeddings::embedding_generator_server::{EmbeddingGenerator, EmbeddingGeneratorServer};
+use embeddings::{EmbeddingReply, EmbeddingRequest};
 use errors::*;
 use fastembed::{TextEmbedding, TokenizerFiles, UserDefinedEmbeddingModel};
 use pgrx::bgworkers::*;
@@ -12,19 +21,17 @@ use tokio::{
 use tokio_stream::wrappers::UnixListenerStream;
 use tonic::{transport::Server, Request, Response, Status};
 
-pub mod embeddings {
-    tonic::include_proto!("embeddings");
+const EXT_NAME: &str = "rag_bge_small_en_v15";
+const SOCKET_NAME_PREFIX: &str = formatcp!("/tmp/.s.pgrag.{EXT_NAME}");
+
+pg_module_magic!();
+
+thread_local! {
+    static PID_CELL: OnceCell<i64> = OnceCell::new();
+    static TEXT_EMBEDDING_CELL: OnceCell<TextEmbedding> = const { OnceCell::new() };
 }
-use embeddings::embedding_generator_server::{EmbeddingGenerator, EmbeddingGeneratorServer};
-use embeddings::{EmbeddingReply, EmbeddingRequest};
-
-mod chunk;
-mod errors;
-
-static SOCKET_PATH: &str = "/tmp/.s.pgrag_bge_small_en_v15";
 
 macro_rules! local_tokenizer_files {
-    // NOTE: macro assumes /unix/style/paths
     ($folder:literal) => {
         TokenizerFiles {
             tokenizer_file: include_bytes!(concat!($folder, "/tokenizer.json")).to_vec(),
@@ -36,7 +43,6 @@ macro_rules! local_tokenizer_files {
 }
 
 macro_rules! local_model {
-    // NOTE: macro assumes /unix/style/paths
     ($model:ident, $folder:literal) => {
         $model {
             onnx_file: include_bytes!(concat!($folder, "/model.onnx")).to_vec(),
@@ -45,28 +51,21 @@ macro_rules! local_model {
     };
 }
 
-#[derive(Debug, Default)]
-pub struct EmbeddingGeneratorStruct {}
-
-pg_module_magic!();
-
-thread_local! {
-    static PID_CELL: OnceCell<i64> = OnceCell::new();
-    static TEXT_EMBEDDING_CELL: OnceCell<TextEmbedding> = const { OnceCell::new() };
-}
-
 #[pg_guard]
 pub extern "C" fn _PG_init() {
     let pid = std::process::id() as i64;
-    PID_CELL.with(|cell| cell.set(pid).expect_or_pg_err("Couldn't store PID"));
+    PID_CELL.with(|cell| cell.set(pid).expect_or_pg_err("Couldn't store socket path"));
 
-    BackgroundWorkerBuilder::new("rag_bge_small_en_v15 embeddings background worker")
-        .set_function("rag_bge_small_en_v15_background_main")
-        .set_library("rag_bge_small_en_v15")
+    BackgroundWorkerBuilder::new(formatcp!("{EXT_NAME} embeddings background worker"))
+        .set_function(formatcp!("background_main"))
+        .set_library(EXT_NAME)
         .set_argument(pid.into_datum())
         .enable_spi_access()
         .load();
 }
+
+#[derive(Debug, Default)]
+pub struct EmbeddingGeneratorStruct {}
 
 #[tonic::async_trait]
 impl EmbeddingGenerator for EmbeddingGeneratorStruct {
@@ -92,25 +91,23 @@ impl EmbeddingGenerator for EmbeddingGeneratorStruct {
 
 #[pg_guard]
 #[no_mangle]
-pub extern "C" fn rag_bge_small_en_v15_background_main(arg: pg_sys::Datum) {
-    // if we don't attach the SIGTERM handler, we'll never be able to exit via an external notification
-    BackgroundWorker::attach_signal_handlers(SignalWakeFlags::SIGTERM);
-
+pub extern "C" fn background_main(arg: pg_sys::Datum) {
     let pid = unsafe { i64::from_polymorphic_datum(arg, false, pg_sys::INT8OID).unwrap_or_pg_err("No PID received") };
     let name = BackgroundWorker::get_name();
-    log!("{name} started by PID {pid}");
+    log!("{name} started, received PID {pid}");
 
+    BackgroundWorker::attach_signal_handlers(SignalWakeFlags::SIGTERM);
     tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()
         .expect_or_pg_err("Couldn't build runtime for server")
         .block_on(async {
-            let path = &format!("{SOCKET_PATH}.{pid}");
-            fs::remove_file(path).unwrap_or_default(); // file may not exist: this is no problem
-            let uds = UnixListener::bind(path).expect_or_pg_err(&format!("Couldn't create socket at {path}"));
-            fs::set_permissions(path, fs::Permissions::from_mode(0o777))
-                .expect_or_pg_err(&format!("Couldn't set permissions for {path}"));
-            log!("{name} created socket {}", path);
+            let path = format!("{SOCKET_NAME_PREFIX}.{pid}");
+            fs::remove_file(&path).unwrap_or_default(); // it's not an error if the file isn't there
+            let uds = UnixListener::bind(&path).expect_or_pg_err(&format!("Couldn't create socket at {}", &path));
+            fs::set_permissions(&path, fs::Permissions::from_mode(0o777))
+                .expect_or_pg_err(&format!("Couldn't set permissions for {}", &path));
+            log!("{} created socket {}", name, &path);
             let uds_stream = UnixListenerStream::new(uds);
             let embedder = EmbeddingGeneratorStruct::default();
 
@@ -118,7 +115,7 @@ pub extern "C" fn rag_bge_small_en_v15_background_main(arg: pg_sys::Datum) {
                 .add_service(EmbeddingGeneratorServer::new(embedder))
                 .serve_with_incoming_shutdown(uds_stream, async {
                     while !BackgroundWorker::sigterm_received() {
-                        sleep(Duration::from_millis(1000)).await;
+                        sleep(Duration::from_secs(1)).await;
                     }
                 })
                 .await
@@ -128,38 +125,12 @@ pub extern "C" fn rag_bge_small_en_v15_background_main(arg: pg_sys::Datum) {
 
 #[pg_schema]
 mod rag_bge_small_en_v15 {
-    use super::errors::*;
-    use super::PID_CELL;
-    use super::SOCKET_PATH;
-    use fastembed::{TextEmbedding, TokenizerFiles, UserDefinedEmbeddingModel};
+    use super::{errors::*, PID_CELL, SOCKET_NAME_PREFIX};
     use hyper_util::rt::TokioIo;
     use pgrx::prelude::*;
-    use std::cell::OnceCell;
     use tokio::net::UnixStream;
     use tonic::transport::{Endpoint, Uri};
     use tower::service_fn;
-
-    macro_rules! local_tokenizer_files {
-        // NOTE: macro assumes /unix/style/paths
-        ($folder:literal) => {
-            TokenizerFiles {
-                tokenizer_file: include_bytes!(concat!($folder, "/tokenizer.json")).to_vec(),
-                config_file: include_bytes!(concat!($folder, "/config.json")).to_vec(),
-                special_tokens_map_file: include_bytes!(concat!($folder, "/special_tokens_map.json")).to_vec(),
-                tokenizer_config_file: include_bytes!(concat!($folder, "/tokenizer_config.json")).to_vec(),
-            }
-        };
-    }
-
-    macro_rules! local_model {
-        // NOTE: macro assumes /unix/style/paths
-        ($model:ident, $folder:literal) => {
-            $model {
-                onnx_file: include_bytes!(concat!($folder, "/model.onnx")).to_vec(),
-                tokenizer_files: local_tokenizer_files!($folder),
-            }
-        };
-    }
 
     use embeddings::embedding_generator_client::EmbeddingGeneratorClient;
     use embeddings::EmbeddingRequest;
@@ -168,8 +139,8 @@ mod rag_bge_small_en_v15 {
         tonic::include_proto!("embeddings");
     }
 
-    #[pg_extern]
-    fn bgwtest(text: &str) -> Vec<f32> {
+    #[pg_extern(immutable, strict)]
+    pub fn _embedding(text: &str) -> Vec<f32> {
         tokio::runtime::Builder::new_current_thread()
             .enable_all()
             .build()
@@ -178,17 +149,13 @@ mod rag_bge_small_en_v15 {
                 let channel = Endpoint::try_from("http://[::]:80") // URL must be valid but is ignored
                     .expect_or_pg_err("Failed to create endpoint")
                     .connect_with_connector(service_fn(|_: Uri| async {
-                        let pid = PID_CELL.with(|cell| {
-                            cell.get()
-                                .unwrap_or_pg_err("Couldn't retrieve PID of launching process")
-                                .clone()
-                        });
+                        let pid = PID_CELL.with(|cell| cell.get().unwrap_or_pg_err("Couldn't get socket name").clone());
+                        let path = format!("{SOCKET_NAME_PREFIX}.{pid}").clone();
 
-                        let path = &format!("{SOCKET_PATH}.{pid}");
                         Ok::<_, std::io::Error>(TokioIo::new(
-                            UnixStream::connect(path)
+                            UnixStream::connect(&path)
                                 .await
-                                .expect_or_pg_err("Couldn't connect embedding worker stream"),
+                                .expect_or_pg_err(&format!("Couldn't connect embedding worker stream {}", &path)),
                         ))
                     }))
                     .await
@@ -203,36 +170,6 @@ mod rag_bge_small_en_v15 {
 
                 response.into_inner().embedding
             })
-    }
-
-    // NOTE. It might be nice to expose this function directly, but as at 2024-07-08 pgrx
-    // doesn't support Vec<Vec<_>>: https://github.com/pgcentralfoundation/pgrx/issues/1762.
-
-    // #[pg_extern(immutable, strict, name = "_embedding")]
-    pub fn embeddings(input: Vec<&str>) -> Vec<Vec<f32>> {
-        thread_local! {
-            static CELL: OnceCell<TextEmbedding> = const { OnceCell::new() };
-        }
-        CELL.with(|cell| {
-            let model = cell.get_or_init(|| {
-                let user_def_model = local_model!(UserDefinedEmbeddingModel, "../../../lib/bge_small_en_v15");
-                TextEmbedding::try_new_from_user_defined(user_def_model, Default::default())
-                    .expect_or_pg_err("Couldn't load model bge_small_en_v15")
-            });
-
-            model
-                .embed(input, None)
-                .expect_or_pg_err("Unable to generate bge_small_en_v15 embeddings")
-        })
-    }
-
-    #[pg_extern(immutable, strict)]
-    pub fn _embedding(input: &str) -> Vec<f32> {
-        let vectors = embeddings(vec![input]);
-        vectors
-            .into_iter()
-            .next()
-            .unwrap_or_pg_err("Unexpectedly empty result vector")
     }
 
     extension_sql!(
