e7932bc27fd95445c17c3dba01e8effccc3af6e6
Elizabeth Murray <52375559+bizwark@users.noreply.github.com>
2025-05-07 07:07:22 -0700

    Change wait_latch to a zero timeout to poll instead of blocking. (#9)
    wait_latch is not async, and will not suspend. Poll and sleep to give other things a chance to run.

--- exts/rag_bge_small_en_v15/src/lib.rs
+++ exts/rag_bge_small_en_v15/src/lib.rs
@@ -18,7 +18,7 @@ use rayon::{ThreadPool, ThreadPoolBuilder};
 use std::{fs, os::unix::fs::PermissionsExt, sync::OnceLock};
 use tokio::{
     net::UnixListener,
-    time::Duration,
+    time::{sleep, Duration},
 };
 use tokio_stream::wrappers::UnixListenerStream;
 use tonic::{transport::Server, Request, Response, Status};
@@ -166,7 +166,10 @@ pub extern "C-unwind" fn background_main(arg: pg_sys::Datum) {
             Server::builder()
                 .add_service(EmbeddingGeneratorServer::new(embedder))
                 .serve_with_incoming_shutdown(uds_stream, async {
-                    while BackgroundWorker::wait_latch(None) {
+                    // wait_latch is not an async function and does not suspend
+                    while BackgroundWorker::wait_latch(Some(Duration::from_secs(0))) {
+                        // suspend so that other asyncs/threads can run
+                        sleep(Duration::from_millis(500)).await;
                     }
                 })
                 .await
--- exts/rag_jina_reranker_v1_tiny_en/src/lib.rs
+++ exts/rag_jina_reranker_v1_tiny_en/src/lib.rs
@@ -17,7 +17,7 @@ use reranking::{
 use std::{fs, os::unix::fs::PermissionsExt, sync::OnceLock};
 use tokio::{
     net::UnixListener,
-    time::Duration,
+    time::{sleep, Duration},
 };
 use tokio_stream::wrappers::UnixListenerStream;
 use tonic::{transport::Server, Request, Response, Status};
@@ -170,7 +170,10 @@ pub extern "C-unwind" fn background_main(arg: pg_sys::Datum) {
             Server::builder()
                 .add_service(RerankerServer::new(reranker))
                 .serve_with_incoming_shutdown(uds_stream, async {
-                    while BackgroundWorker::wait_latch(None) {
+                    // wait_latch is not an async function and does not suspend
+                    while BackgroundWorker::wait_latch(Some(Duration::from_secs(0))) {
+                        // suspend so that other asyncs/threads can run
+                        sleep(Duration::from_millis(500)).await;
                     }
                 })
                 .await
